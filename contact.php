2a32b80f55bd6613ea33a6030f76d6fe1d754332
anhthu24 <100059258+anhthu24@users.noreply.github.com>
2024-09-03 00:50:23 +0700

    upload

---
+++ contact.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://use.fontawesome.com/2145adbb48.js"></script>
+    <script src="https://kit.fontawesome.com/a42aeb5b72.js" crossorigin="anonymous"></script>
+    <title>Thông tin liên hệ</title>
+</head>
+
+<body>
+    <nav>
+        <label class="logo">BOOKSTORE</label>
+        <ul>
+            <li><a href="index.php">Trang chủ</a></li>
+            <li><a href="productList.php">Sản phẩm</a></li>
+            <li><a href="contact.php" class="active">Liên hệ</a></li>
+            <li><a href="register.php" id="signup">Đăng ký</a></li>
+            <li><a href="login.php" id="signin" >Đăng nhập</a></li>
+            <li><a href="order.php" id="order">Đơn hàng</a></li>
+            <li>
+                <a href="checkout.php">
+                    <i class="fa fa-shopping-bag"></i>
+                    <span class="sumItem">
+                        0
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <section class="banner"></section>
+    <div class="featuredProducts">
+        <h1>Thông tin liên hệ</h1>
+    </div>
+    <div class="container-single">
+        <div>
+        <p>Bookstore là một trong những hệ thống website bán sách hàng đầu. 
+         Với nhiều gian hàng của các nhà xuất bản sách nổi tiếng như Alphabooks, First News, Vinabook…bản quyền, chất lượng, uy tín, với nhiều chính sách ưu đãi.<br>
+
+         Hiện tại có trên 30,000 đầu sách với đầy đủ các chủng loại sách, chắc chắn Bookstore sẽ đáp ứng đầy đủ các nhu cầu và sở thích đọc sách phong phú của bạn.
+        <br>
+          Liên hệ với chúng tôi :<br>
+          - Địa chỉ: 600 Tân Kỳ Tân Quý, phường Bình Hưng Hòa A, quận Bình Tân.<br>
+          - Email:<a href="#">support@bookstore.com</a><br>
+          -  Điện thoại: 0367041460.
+       </p> 
+        </div>
+         <div class="contact_map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.230880653988!2d106.60621031376849!3d10.793620961830044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752c0a87c015a9%3A0xa6e84e00b0822b43!2zNjAwIFTDom4gS-G7syBUw6JuIFF1w70sIELDrG5oIEjGsG5nIEhvw6AgQSwgQsOsbmggVMOibiwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1666400807721!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
+         </div>
+        
+       
+    </div>
+    </div>
+    <footer>
+        <div class="social">
+            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+        <ul class="list">
+            <li>
+                <a href="./">Trang Chủ</a>
+            </li>
+            <li>
+                <a href="productList.php">Sản Phẩm</a>
+            </li>
+            <li>
+                <a href="contact.php">Liên hệ</a>
+            </li>
+        </ul>
+        <p class="copyright">BOOKSTORE @ 2022</p>
+    </footer>
+</body>
+
+</html>
